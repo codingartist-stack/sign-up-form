182ed87e34c297fc80d9109e3112daee87526037
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-03-15 20:14:31 -0600

    forgot to link the JavaScript file to the html. Used text content instead of value to compare the passwords. tutor helped get me on the right track. got it working.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,33 @@
 const password = document.getElementById("password");
 const confirmPass = document.getElementById("confirmpassword");
+const error = document.getElementById("error");
+
+function checkPass(e) {
+    if (password.value === confirmPass.value) {
+        password.style.borderColor = "green"
+        confirmPass.style.borderColor = "green"
+    } else {
+        password.style.borderColor = "red"
+        confirmPass.style.borderColor = "red"
+        error.textContent = "* Passwords do not match"
+        error.style.color = "red"
+    }
+}
+
+
+confirmPass.addEventListener("input", (e) => {
+    console.log('am I working?');
+    checkPass(e)
+})
+
+
+//grab password and confirm pass
+//check if they are the same
+//if same boarder green
+//if not boarder red -- should it be red if they are both empty?
+//display error. 
+
+//buckets and loops
+
+//password bucket is password
 
